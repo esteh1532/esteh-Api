20f0388dcb9ff9cbcd6b7641be99ea26c88ba7ce
esteh1532 <130610158+esteh1532@users.noreply.github.com>
2023-05-15 01:51:15 +0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,7 +1,7 @@
 const fs = require('fs')
 
-global.creator = 'Zeeoneofc' 
-global.apikey = ["Zeeoneofc", "Alphabot", "Alpha"]
+global.creator = 'teh plastik' 
+global.apikey = ["esteh"]
 
 let file = require.resolve(__filename)
 fs.watchFile(file, () => {
